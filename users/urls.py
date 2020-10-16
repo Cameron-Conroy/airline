5ba3c52d9a0bf0ac3326064023e1e7f029d55ff6
Cameron-Conroy <cameron.conroy.cc@gmail.com>
2020-10-16 11:35:32 -0400

    adding login logic

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login")
-    # path("logout", views.logout_view, name="logout")
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
 # three different routes
 # one index route
