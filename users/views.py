764b164c6a6731b1ac308dbcc223563e96bf41ff
Cameron-Conroy <cameron.conroy.cc@gmail.com>
2020-10-16 09:53:12 -0400

    try to fix server bug

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
+from django.http import request
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+
+def login_view(request):
+    return render(request, "user/login.html")
+
+
+def logout_view(request):
+    pass
