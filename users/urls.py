bb636eb1ea8ce0bacb6c1d6accfa358ba6097677
Cameron-Conroy <cameron.conroy.cc@gmail.com>
2020-10-16 08:28:15 -0400

    added users app -> initial commit

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.urls.resolvers import RoutePattern
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
+]
+# three different routes
+# one index route
+# one login route
+# one logout route
+# all three of these paths need a function
